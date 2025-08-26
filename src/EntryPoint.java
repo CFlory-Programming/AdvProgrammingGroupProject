2df48574549d810ec44bfce71bcbe503afb40d87
Jonwu25 <425443@graniteschools.org>
2025-08-25 19:43:16 -0600

    Add files via upload

---
+++ src/EntryPoint.java
@@ -0,0 +1,10 @@
+public class EntryPoint
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Donkey Kong Game");
+        System.out.println("Coded by Cole Flory, Emil Gruenwald, and Jonathan Wu\n");
+        
+        System.out.println("This is a timing-oriented platformer game, with entertaining mechanics such as launch barrels and special collectibles. Each handcrafted level is fun and engaging; they are guaranteed to challenge most players.");
+    }
+}
