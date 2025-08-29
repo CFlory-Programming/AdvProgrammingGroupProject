342babd1adf994e44e92aaa58d1edcdf3a4a9ee3
Emil-Gruenwald <142806438+Emil-Gruenwald@users.noreply.github.com>
2025-08-29 12:25:58 -0600

    Add files via upload

--- src/DonkeyKongGame.java
+++ src/DonkeyKongGame.java
@@ -2,9 +2,22 @@ public class DonkeyKongGame
 {
     public static void main(String[] args)
     {
+
+        Player p1 = new Player();
+        List<Enemy> enemies = new ArrayList<>();
+        List<Coin> coins = new ArrayList<>();
+        List<LevelObject> objects = new ArrayList<>();
+
+        int score = 0;
+        int level = 0;
+
         System.out.println("Donkey Kong Game");
         System.out.println("Coded by Cole Flory, Emil Gruenwald, and Jonathan Wu\n");
         
         System.out.println("This is a timing-oriented platformer game, with entertaining mechanics such as launch barrels and special collectibles. Each handcrafted level is fun and engaging; they are guaranteed to challenge most players.");
+    }
+
+    public void display() {
+        
     }
 }
