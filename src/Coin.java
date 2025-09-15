43ab326f942c7f2721bf995cba42bed87b0ebec6
Jonwu25 <425443@graniteschools.org>
2025-09-15 11:42:45 -0600

    Update Coin.java

--- src/Coin.java
+++ src/Coin.java
@@ -4,6 +4,17 @@ public class Coin
     {
         int x = 0;
         int y = 0;
+        int value = 1;
+    }
+
+    public boolean isCollected()
+    {
+        return true;
+    }
+
+    public int getValue()
+    {
+        return value;
     }
     
     public void display()
