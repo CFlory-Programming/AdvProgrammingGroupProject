4971a2db374c0d6a2bc9ac6a67fd8e03741ddb9b
Emil-Gruenwald <gruenwald.emil@gmail.com>
2025-09-29 11:59:59 -0600

    Update DonkeyKongGame.java

--- src/Java/src/DonkeyKongGame.java
+++ src/Java/src/DonkeyKongGame.java
@@ -17,10 +17,10 @@ public class DonkeyKongGame extends PApplet
         int score = 0;
         int level = 0;
 
-        System.out.println("Donkey Kong Game");
-        System.out.println("Coded by Cole Flory, Emil Gruenwald, and Jonathan Wu\n");
+        //System.out.println("Donkey Kong Game");
+        //System.out.println("Coded by Cole Flory, Emil Gruenwald, and Jonathan Wu\n");
         
-        System.out.println("This is a timing-oriented retro style 2D platformer game, with entertaining mechanics such as launch barrels and special collectibles. Each handcrafted level is fun and engaging; they are guaranteed to challenge most players. This game is referenced from Donkey Kong Country and Super Mario Brothers Wii. This game's features will be picked and chosen from each game, including the most fun aspects from each. Our platformer is created with a tile based level design, each level being created by hand for the most fun challanges.");
+        //System.out.println("This is a timing-oriented retro style 2D platformer game, with entertaining mechanics such as launch barrels and special collectibles. Each handcrafted level is fun and engaging; they are guaranteed to challenge most players. This game is referenced from Donkey Kong Country and Super Mario Brothers Wii. This game's features will be picked and chosen from each game, including the most fun aspects from each. Our platformer is created with a tile based level design, each level being created by hand for the most fun challanges.");
     }
 
     public void display()
