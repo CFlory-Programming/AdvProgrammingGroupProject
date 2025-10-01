6b498d45e55ecff3406cdbb0945c5280a96d344a
CFlory-Programming <108110155+CFlory-Programming@users.noreply.github.com>
2025-09-30 19:16:34 -0600

    Draw logic for Coin class

--- src/Java/src/DonkeyKongGame.java
+++ src/Java/src/DonkeyKongGame.java
@@ -1,9 +1,8 @@
-import java.util.ArrayList;
 import processing.core.PApplet;
 
 public class DonkeyKongGame extends PApplet
 {
-    // static reference to the running Processing sketch so plain model classes can draw
+    // static reference to the running Processing sketch so other classes can draw
     public static PApplet sketch;
 
     // shared coins list so main() can populate it before the sketch starts
@@ -11,9 +10,9 @@ public class DonkeyKongGame extends PApplet
     public static void main(String[] args)
     {
         // create a couple of test coins before starting the Processing sketch
-        coins.add(new Coin(100, 100));
-        coins.add(new Coin(200, 150));
-        coins.add(new Coin(300, 80));
+        coins.add(new Coin(100, 100, 1));
+        coins.add(new Coin(200, 150, 1));
+        coins.add(new Coin(300, 80, 1));
 
         // start the Processing sketch
         PApplet.main("DonkeyKongGame");
@@ -32,21 +31,29 @@ public class DonkeyKongGame extends PApplet
 
     @Override
     public void setup() {
+        // store reference to sketch so other classes can draw
         // Set initial background color
-        // store reference so model classes can draw
         sketch = this;
         background(255);
     }
 
     @Override
     public void draw() {
-        // Draw a moving circle
+        // clear
         background(255);
 
-        // draw coins added from main/setup
-        for (Coin c : coins) {
+        // draw coins added from main/setup and compute total value
+        int totalCoins = 0;
+        for (Coin c : coins) { // for each loop
             // call the coin's display method which will use the static sketch reference
             c.display();
+            totalCoins += c.getValue();
         }
+
+        // draw total value in red at the top-right corner
+        fill(255, 0, 0);
+        textAlign(RIGHT, TOP);
+        textSize(16);
+        text("Total value: " + totalCoins, width - 10, 10); //x is 10px from the right edge, y is 10px from the top
     }
 }
