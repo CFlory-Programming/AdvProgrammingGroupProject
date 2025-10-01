694ae105b4e20099ae61982935ea726680748c99
Emil-Gruenwald <142806438+Emil-Gruenwald@users.noreply.github.com>
2025-09-30 19:56:16 -0600

    Update Player.java

--- src/Java/src/Player.java
+++ src/Java/src/Player.java
@@ -7,30 +7,45 @@ public class Player
     int y;
     int score;
     int lives;
+    float speedX;
+    float speedY;
+    boolean inAir;
 
-    public Player()
+
+    public Player(int height, int width, int x, int y, int score, int lives)
     {
-        height = 0;
-        width = 0;
-        x = 0;
-        y = 0;
-        score = 0;
-        lives = 3;
+        this.height = height;
+        this.width = width;
+        this.x = x;
+        this.y = y;
+        this.score = score;
+        this.lives = lives;
+        speedX = 0;
+        speedY = 0;
+        inAir = false;
     }
     
     public void jump()
     {
-            
+        speedY -= 20;
     }
     
-    public void walk()
+    public void walk(char direction)
     {
-        
+        if (direction == 'r') {
+            speedX += 5;
+        } else if (direction == 'l') {
+            speedX -= 5;
+        }
     }
     
-    public void run()
+    public void run(char direction)
     {
-        
+        if (direction == 'r') {
+            speedX += 8;
+        } else if (direction == 'l') {
+            speedX -= 8;
+        }
     }
     
     public void interact()
@@ -38,8 +53,50 @@ public class Player
         
     }
     
-    public void display()
+    public void display(int camX, int camY)
     {
-        
+        if (DonkeyKongGame.sketch != null) {
+            DonkeyKongGame.sketch.noStroke();
+            DonkeyKongGame.sketch.fill(255,0,0);
+            DonkeyKongGame.sketch.rect(x - camX, y - camY, width, height);
+        }
+    }
+
+    public void update(int[][] tiles)
+    {
+        // Update player position based on speed
+        speedY += 0.5; // Gravity
+
+        // Collision detection for x direction
+        x += speedX;
+        boolean collideX = checkCollision(x, y + height/2, 50, tiles) || checkCollision(x, y, 50, tiles) || (checkCollision(x, y + height, 50, tiles) && y%50!=0) || checkCollision(x + width, y + height/2, 50, tiles) || checkCollision(x + width, y, 50, tiles) || (checkCollision(x + width, y + height, 50, tiles) && y%50!=0);
+        if (collideX) {
+            if(speedX>0) {
+              x = 50*(x/50);
+            } else if(speedX<0) {
+              x = 50*(x/50)+50;
+            }
+            speedX = 0;
+        }
+
+        // Collision detection for y direction
+        y += speedY;
+        boolean collideY = checkCollision(x, y + height, 50, tiles) || (checkCollision(x + width, y + height, 50, tiles) && x%50!=0) || checkCollision(x, y, 50, tiles) || (checkCollision(x + width, y, 50, tiles) && x%50!=0);
+        if (collideY) {
+            if(speedY>0){
+              y = 50*(y/50);
+            } else if(speedY<0){
+              y = 50*(y/50)+50;
+            }
+            speedY = 0;
+            inAir = false;
+        } else {
+            inAir = true;
+        }
+    }
+
+    private boolean checkCollision(int tileX, int tileY, int tileSize, int[][] tiles)
+    {
+        return tiles[tileX / tileSize][tileY / tileSize] == 1;
     }
 }
