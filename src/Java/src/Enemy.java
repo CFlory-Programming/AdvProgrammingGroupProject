16e58809b020f27dfacb05e42f1599ca1cdc8452
Jonwu25 <425443@graniteschools.org>
2025-11-24 11:43:11 -0700

    Moved enemy ai to respective classes

--- src/Java/src/Enemy.java
+++ src/Java/src/Enemy.java
@@ -92,22 +92,7 @@ public class Enemy
 
     public void ai(int[][] tiles, Player p1, int[] collisionTiles, ArrayList<Enemy> enemies)
     {
-        if (p1.x > x) {
-            if (p1.x-x >= 4){
-                move(4, 'r', false);
-            } else if (!collideX(p1.x, y, tiles, collisionTiles)) {
-                move(p1.x - x, 'r', false);
-            }
-        } else if (p1.x < x) {
-            if (x-p1.x >= 4){
-                move(4, 'l', false);
-            } else if (!collideX(p1.x, y, tiles, collisionTiles)) {
-                move(p1.x - x, 'r', false);
-            }
-        }
-        if (!inAir && ((p1.y <= y && !collideY((int)(x+speedX), (int)(y+1), tiles, collisionTiles)) || (x%50==0 && (collideX(x+1, y, tiles, collisionTiles) || collideX(x-1, y, tiles, collisionTiles))))) {
-            jump(10, 'u');
-        }
+        // Placeholder for enemy AI behavior
     }
 
     public boolean collideX(int fx, int fy, int[][] tiles, int[] collisionTiles)
