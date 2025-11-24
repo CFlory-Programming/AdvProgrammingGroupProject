0f14b790605d86540e8558c397df9aa005902891
Emil-Gruenwald <gruenwald.emil@gmail.com>
2025-11-24 08:07:13 -0700

    Create PowerUp.java

---
+++ src/Java/src/PowerUp.java
@@ -0,0 +1,51 @@
+import processing.core.PImage;
+
+public class PowerUp extends LevelObject{
+
+    public boolean hasTimer;
+    public int timerDuration; // in milliseconds
+    public boolean disapearsOnHit;
+    public int timeCollected; // in milliseconds
+    public boolean isActive;
+
+    public PowerUp(PImage sprite, int x, int y, int height, int width, boolean hasTimer, int timerDuration, boolean disapearsOnHit) {
+        super(sprite, x, y, height, width);
+        this.hasTimer = hasTimer;
+        this.timerDuration = timerDuration;
+        this.disapearsOnHit = disapearsOnHit;
+
+        timeCollected = 0;
+        isActive = false;
+    }
+
+
+    public void update(Player player) {
+        if (collidesWith(player) && !isActive) {
+            isActive = true;
+            timeCollected = KonQuestGame.sketch.millis();
+            applyEffect(player);
+        }
+
+        if (isActive && hasTimer) {
+            int currentTime = KonQuestGame.sketch.millis();
+            if (currentTime - timeCollected >= timerDuration) {
+                removeEffect(player);
+                isActive = false;
+            }
+        }
+
+        if (player.attacked && disapearsOnHit && isActive) {
+            removeEffect(player);
+            isActive = false;
+        }
+
+    }
+
+    public void applyEffect(Player player) {
+        // To be overridden by subclasses
+    }
+
+    public void removeEffect(Player player) {
+        // To be overridden by subclasses
+    }
+}
