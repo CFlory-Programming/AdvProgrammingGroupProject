1a9538a4b7bc6b0b60a59fd316bfe43664dec894
Emil-Gruenwald <gruenwald.emil@gmail.com>
2025-11-24 11:25:42 -0700

    Update SpeedBoost.java

--- src/Java/src/SpeedBoost.java
+++ src/Java/src/SpeedBoost.java
@@ -4,8 +4,8 @@ public class SpeedBoost extends PowerUp {
 
     private float speedMultiplier;
 
-    public SpeedBoost(PImage sprite, int x, int y, int height, int width, boolean hasTimer, int timerDuration, boolean disapearsOnHit, float speedMultiplier) {
-        super(sprite, x, y, height, width, hasTimer, timerDuration, disapearsOnHit);
+    public SpeedBoost(PImage sprite, int x, int y, int height, int width, float speedMultiplier) {
+        super(sprite, x, y, height, width, true, 10000, true);
         this.speedMultiplier = speedMultiplier;
     }
 
