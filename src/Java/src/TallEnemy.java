233c98e24e502a9a6cc6f6c6b6b61c762bea9704
Jonwu25 <425443@graniteschools.org>
2025-11-24 11:30:04 -0700

    Create TallEnemy.java

---
+++ src/Java/src/TallEnemy.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class TallEnemy extends Enemy
+{
+    public TallEnemy(int x, int y)
+    {
+        super(x, y);
+        height = 100;
+        width = 50;
+    }
+}
