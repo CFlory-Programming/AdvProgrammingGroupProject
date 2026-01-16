2c030500327e9dff33fdf361ba1bf040855501cc
Jonwu25 <226426814+Jonwu25@users.noreply.github.com>
2026-01-16 11:07:34 -0700

    Arrows slowly fade out after collision

--- src/Java/src/Arrow.java
+++ src/Java/src/Arrow.java
@@ -1,9 +1,17 @@
 public class Arrow extends AbstractProjectile {
+    int timer = 0;
     public Arrow(float x, float y, float direction, float speed, int width, int height) {
         super(x, y, direction, speed, width, height);
     }
 
     public void update() {
+        if (timer >= 300) {
+            exists = false;
+        }
+        if (speedX == 0 && speedY == 0) {
+            timer++;
+            return;
+        }
         physics();
         if (checkHit(KonQuestGame.tiles, KonQuestGame.collisionTiles)) {
             speedX = 0;
@@ -21,9 +29,6 @@ public class Arrow extends AbstractProjectile {
     }
 
     public void physics() {
-        if (speedX == 0 && speedY == 0) {
-            return;
-        }
         // Gravity effect
         speedY += 0.1;
         direction = (float) Math.atan2(speedY, speedX);
@@ -33,7 +38,7 @@ public class Arrow extends AbstractProjectile {
         KonQuestGame.sketch.pushMatrix();
         KonQuestGame.sketch.translate((float) (x - camX), (float) (y - camY));
         KonQuestGame.sketch.rotate(direction);
-        KonQuestGame.sketch.fill(150, 75, 0);
+        KonQuestGame.sketch.fill(150, 75, 0, 255 - (int)((timer / 300.0) * 255));
         KonQuestGame.sketch.rect(-width/2, -height/2, width, height);
         KonQuestGame.sketch.popMatrix();
     }
